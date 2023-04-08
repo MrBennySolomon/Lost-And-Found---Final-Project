63b9557c8b984e4cc5d30d75195025b39f6a54d7
MrBennySolomon <MrBennySolomon@gmail.com>
2023-04-08 16:22:07 +0300

    final-project add minion 3.5

--- src/MVC/View/pages/FindItem.js
+++ src/MVC/View/pages/FindItem.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import                                 '../../../css/FindItem.css';
 import React, { useEffect }                      from 'react';
 import { Unity, useUnityContext } from "react-unity-webgl";
