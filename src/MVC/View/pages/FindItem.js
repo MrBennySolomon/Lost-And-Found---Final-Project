eb078d49a8d75aa8000121214dce355d3fb1ee95
MrBennySolomon <MrBennySolomon@gmail.com>
2023-04-18 09:29:54 +0300

    add unity progress bar

--- src/MVC/View/pages/FindItem.js
+++ src/MVC/View/pages/FindItem.js
@@ -7,7 +7,7 @@ import { useParams }              from 'react-router-dom';
 const FindItem = () => {
   const params = useParams();
   
-  const { unload, unityProvider } = useUnityContext({
+  const { unload, unityProvider, loadingProgression  } = useUnityContext({
     loaderUrl:    `https://lost-and-found-server-5v26.onrender.com/uploads/${params.name}.loader.js`,
     dataUrl:      `https://lost-and-found-server-5v26.onrender.com/uploads/${params.name}.data.unityweb`,
     frameworkUrl: `https://lost-and-found-server-5v26.onrender.com/uploads/${params.name}.framework.js.unityweb`,
@@ -27,8 +27,22 @@ const FindItem = () => {
   return (
     <>
       <div className='find'>
-        <Unity unityProvider={unityProvider}/>      
+        <Unity unityProvider={unityProvider}/>   
       </div>
+      <div className="progress mt-2"
+        style={{margin: '0 auto', textAlign: 'center',border: '3px solid #2991EA', width: '60%', height: '2.6rem'}}
+        >
+          <div className="progress-bar" 
+          role='progressbar'
+          aria-valuenow={loadingProgression * 100}
+          aria-valuemin='0'
+          aria-valuemax='100'
+          style={{height: '2rem', background: '#2991EA', color: 'white', width: `${loadingProgression * 100}%`}}>
+            {`${loadingProgression * 100}%`}
+          
+          </div>
+
+        </div>
       <button onClick={handleClickBack}>Press To Unload Unity Before Leaving This Page</button>
     </>
   )
